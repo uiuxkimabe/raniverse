6d32836287977e9be145a1b5a4dec4b732c80742
uiuxkimabe <uiuxkimabe@gmail.com>
2025-08-07 20:56:53 +0700

    five commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,10 @@
 const cta = document.querySelector('.cta');
-const openingMsg = document.querySelector('.message');
 const playSertaMulia = document.querySelector('#sertamulia');
-const nextBtntoJumbotron = document.querySelector('main .message figcaption a');
-const openingVideo = document.querySelector('main .message video')
-const pageJumbotron = document.querySelector('#jumbotron');
+const mainOpen = document.querySelector('main');
 cta.addEventListener('click',()=> {
+   mainOpen.classList.add('active')
    playLaguSertaMuliaSegment();
-   openingMsg.classList.add('active');
-   openingMsg.style.width = "100%";
-   openingMsg.style.height = "90%";
-   setTimeout(() => {
-      pageJumbotron.style.display = 'block';
-      pageJumbotron.style.height = '100vh';
-      nextBtntoJumbotron.style.display = 'block';
-      // openingVideo.removeAttribute('autoplay');
-      // openingVideo.pause();
-   }, 65000);
+   
 })
 
 // Function Lagu Serta Mulia Di Mulai
