ad86f968d7e372e911a72dd722752c5a199c5ff5
uiuxkimabe <uiuxkimabe@gmail.com>
2025-08-09 00:30:33 +0700

    page add

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,27 @@
 const cta = document.querySelector('.cta');
 const playSertaMulia = document.querySelector('#sertamulia');
 const mainOpen = document.querySelector('main');
+const jumbotron = document.querySelector('#jumbotron');
+const ctaNext = document.querySelector('.cta-next');
+const about = document.querySelector('#about');
 cta.addEventListener('click',()=> {
    mainOpen.classList.add('active')
-   playLaguSertaMuliaSegment();
+   playLaguSertaMuliaSegment(117, 248);
+   setTimeout(() => {
+      jumbotron.style.display = 'block';
+   }, 2000);
+   setTimeout(() => {
+      ctaNext.style.display = 'block';
+      about.style.display = 'flex';
+   }, 65000);
    
 })
 
 // Function Lagu Serta Mulia Di Mulai
-function playLaguSertaMuliaSegment() {
-   playSertaMulia.currentTime = 117;
+function playLaguSertaMuliaSegment(start, stop) {
+   playSertaMulia.currentTime = start;
    playSertaMulia.play();
-   const stopAt = 248;
+   const stopAt = stop;
    function onTimeUpdate() {
       if (playSertaMulia.currentTime >= stopAt) {
          playSertaMulia.pause();
