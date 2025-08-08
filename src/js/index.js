86a2bd904594ccc6f64c951d04214ca39b894cf6
uiuxkimabe <uiuxkimabe@gmail.com>
2025-08-09 02:13:43 +0700

    confetti

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ cta.addEventListener('click',()=> {
    setTimeout(() => {
       ctaNext.style.display = 'block';
       about.style.display = 'block';
-   }, 65000);
+   }, 10000);
    
 })
 
@@ -29,4 +29,47 @@ function playLaguSertaMuliaSegment(start, stop) {
       }
    }
    playSertaMulia.addEventListener('timeupdate', onTimeUpdate);
+}
+
+// Confetti
+function startConfetti() {
+   const duration = 15 * 1000,
+   animationEnd = Date.now() + duration;
+
+   let skew = 1;
+
+   function randomInRange(min, max) {
+   return Math.random() * (max - min) + min;
+   }
+
+   (function frame() {
+   const timeLeft = animationEnd - Date.now(),
+      ticks = Math.max(200, 500 * (timeLeft / duration));
+
+   skew = Math.max(0.8, skew - 0.001);
+
+   confetti({
+      particleCount: 1,
+      startVelocity: 0,
+      ticks: ticks,
+      origin: {
+         x: Math.random(),
+         // since particles fall down, skew start toward the top
+         y: Math.random() * skew - 0.2,
+      },
+      colors: ["#ffffff"],
+      shapes: ["circle"],
+      gravity: randomInRange(0.4, 0.6),
+      scalar: randomInRange(0.4, 1),
+      drift: randomInRange(-0.4, 0.4),
+   });
+
+   if (timeLeft > 0) {
+      requestAnimationFrame(frame);
+   }
+   })();
+}
+
+ctaNext.addEventListener('click', () => {
+   startConfetti();
 }
